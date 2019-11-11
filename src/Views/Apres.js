385ded3b6ea4e95c81b57a0443067499001f1136
evertfreit77 <evertonfreitas59@hotmail.com>
2019-11-11 13:01:48 -0300

    Inclusão da lib que faz a conecxão com o broker MQTT
    Eu coloquei dentro da view Apres.js.
    Agora basta setar o host, a porta e o user.
    Outro detalhe: Vcs devem ler mais abaixo no código da Views/Apres.js que tem a função e um tópico no qual ele vai se inscrever, eu não coloquei pq eu não sei o nome do tópico q vc vão criar aí, ou se já criaram. Lá tá igual ao exemplo.
    Mas eu acho q essa lib não vai funcionar pra Amazon

--- src/Views/Apres.js
+++ src/Views/Apres.js
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import { StyleSheet, View, Dimensions, Image } from 'react-native';
+import { StyleSheet, View, Dimensions, Image, AsyncStorage } from 'react-native';
 import { DefaultTheme, Button, Provider as PaperProvider} from 'react-native-paper';
 
 import { LineChart, BarChart, PieChart, ProgressChart, ContributionGraph, StackedBarChart } from "react-native-chart-kit";
+
+import init from 'react_native_mqtt';
 
 import logo from '../assets/Logo.png'
 import Header from '../Components/Header';
@@ -15,16 +17,84 @@ const theme = {
   },
 };
 
+const host = 'link-do-host-aws.com';
+const port = 8000;
+user = 'app';
 
-export default class Apres extends React.Component {
 
-  render(){
+init({
+  size: 10000,
+  storageBackend: AsyncStorage,
+  defaultExpires: 1000 * 3600 * 24,
+  enableCache: true,
+  sync: {},
+});
+
+
+export default function Apres() {
+
+    const client = new Paho.MQTT.Client(host, port, user);
+
+    onMessageArrived = (entry) => {
+      console.log("onMessageArrived:"+entry);
+      console.log(entry.payloadString);
+      myJSON = JSON.parse(entry.payloadString)
+      console.log(myJSON["teste"])
+    }
+
+    onConnectionLost = (err) => {
+      console.log('Connection lost!')
+      console.log(err)
+    }
+
+    onConnect = () => {
+      console.log("Connected!!!!");
+      client.subscribe('hello/world');
+    };
+
+    sendMessage = (message) => {
+      message = new Paho.MQTT.Message(message);
+      message.destinationName = "hello/world";
+      client.send(message);
+
+    //   if(this.state.isConnected){
+    //     this.state.client.send(message);    
+    //   }else{
+    //     this.connect(this.state.client)
+    //       .then(() => {
+    //         this.state.client.send(message);
+    //         this.setState({error: '', isConnected: true});
+    //       })
+    //       .catch((error)=> {
+    //         console.log(error);
+    //         this.setState({error: error});
+    //       });
+    // }
+    }
+
+    const test = () => {
+      console.log('teste')
+      // const client = new Paho.MQTT.Client('broker.mqttdashboard.com', 8000, 'clientId-wZxsgaYWvV',);
+      client.onMessageArrived = onMessageArrived;
+      client.onConnectionLost = onConnectionLost;
+      client.connect({ 
+        onSuccess: onConnect,
+        useSSL: false ,
+        userName: 'yourUser',
+        password: 'yourPass',
+        onFailure: (e) => {console.log("here is the error" , e); }
+  
+      });
+    }
+
     return (
       <View style={styles.container}>
         
+        {test()}
+
         <Image source={logo} style={{ marginBottom: 40, width: '55%', height: 100 }} resizeMode="contain"/>
 
-        <Button style={{ width: '100%', marginBottom: 40, backgroundColor: '#019DDE' }} icon="lightbulb" mode="contained" onPress={() => console.log('Pressed')}>
+        <Button style={{ width: '100%', marginBottom: 40, backgroundColor: '#019DDE' }} icon="lightbulb" mode="contained" onPress={() => {sendMessage('{"teste": "Testando..."}')}}>
 
         </Button>
 
@@ -77,8 +147,8 @@ export default class Apres extends React.Component {
 
       </View>  
     );
-  }
-}
+  
+};
 
 
 const styles = StyleSheet.create({
@@ -89,4 +159,3 @@ const styles = StyleSheet.create({
     
   },
 });
-
